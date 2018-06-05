a119bd2c018bfa7f048ccdff689ce72658a45174
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-05 22:59:55 +0530

    Create base for any MERN stack app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -13,9 +13,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Add a proxy to client's package.json with text:
+          "http://localhost:PORT"
+        </p>
+        <p>Here PORT will be the port that backend is running on. (3001)</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
