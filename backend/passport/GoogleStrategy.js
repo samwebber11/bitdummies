f71f33abe5c7b785760d94ed174176ef39986137
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-09 22:09:44 +0530

    Set up passport and Google strategy for auth

---
+++ backend/passport/GoogleStrategy.js
@@ -0,0 +1,55 @@
+import { OAuth2Strategy } from 'passport-google-oauth'
+
+import keys from '../config/keys'
+import User from '../database/models/user'
+
+const GoogleStrategy = new OAuth2Strategy(
+  {
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
+    callbackURL: '/auth/google/redirect',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // Testing.
+    console.log('============ GOOGLE PROFILE ============')
+    console.log(profile)
+    console.log('============ END ============')
+
+    const { displayName, id } = profile
+    User.findOne({ googleID: id })
+      .then(currentUser => {
+        // If a user already exists.
+        if (currentUser) {
+          done(null, currentUser)
+        } else {
+          // If there is no user in the db with that googleID.
+          console.log('====== PRE SAVE =======')
+          console.log(displayName)
+          console.log(id)
+          console.log(profile)
+          console.log('====== POST SAVE =======')
+          const user = new User({
+            username: displayName,
+            googleID: id,
+            thumbnail: profile._json.image.url,
+          })
+
+          user
+            .save()
+            .then(newUser => {
+              done(null, newUser)
+            })
+            .catch(err => {
+              console.log('Error! Saving the new Google user: ', err)
+              return done(null, false)
+            })
+        }
+      })
+      .catch(err => {
+        console.log('Error! Trying to find user with googleID: ', err)
+        return done(null, false)
+      })
+  }
+)
+
+export default GoogleStrategy
---
+++ backend/passport/index.js
@@ -0,0 +1,32 @@
+import passport from 'passport'
+
+import GoogleStrategy from './GoogleStrategy'
+import User from '../database/models/user'
+
+passport.serializeUser((user, done) => {
+  console.log('*** serializeUser called ***')
+  console.log(user)
+  console.log('---------------')
+  done(null, { _id: user._id })
+})
+
+passport.deserializeUser((id, done) => {
+  console.log('*** deserializeUser called ***')
+  console.log(id)
+  User.findOne({ _id: id }, 'username')
+    .then(user => {
+      console.log('======= USER FOUND =======')
+      console.log(user)
+      console.log('======= END =======')
+      done(null, user)
+    })
+    .catch(err => {
+      console.log('Error in findOne in deserializeUser: ', err)
+      done(null, false)
+    })
+})
+
+// Register strategy.
+passport.use(GoogleStrategy)
+
+export default passport
