da081b3e1bca89cfee7da149b21680532ee79c4d
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-09 22:08:39 +0530

    Use express-session to store the session for a user

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,44 @@
 import express from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
+import session from 'express-session'
 
-import router from './routes'
+import authRouter from './routes/auth'
+import passport from './passport'
+import keys from './config/keys'
+import './database'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+// Middleware.
 app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(
+  session({
+    secret: keys.session.secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 
-app.use('/api', router)
+// Initialize Passport.
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Routes.
+app.use('/auth', authRouter)
+app.get('/', (req, res) => {
+  res.redirect('http://localhost:3000/')
+})
+
+// Error handler.
+app.use((err, req, res, next) => {
+  console.log('====== ERROR =======')
+  console.error(err.stack)
+  res.status(500)
+})
 
 app.listen(PORT, () => {
   console.log(`Express App listening on port ${PORT}`)
