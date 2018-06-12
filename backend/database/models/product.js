a7d19f030665d9c18438fefebb44a50bf6c9a96a
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-12 15:46:12 +0530

    Create a model for storing product details

---
+++ backend/database/models/product.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose.Schema
+
+const ProductSchema = new Schema({
+  category: {
+    type: String,
+    required: true,
+    enum: ['action figure', 'none'],
+    default: 'none',
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+  image: [
+    {
+      path: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  delicacy: {
+    type: String,
+    required: true,
+    enum: ['low', 'medium', 'high'],
+    default: 'high',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+})
+
+const Product = mongoose.model('Product', ProductSchema)
+export default Product
