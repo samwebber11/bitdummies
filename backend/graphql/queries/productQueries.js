2e1df7ce8bc21ff66444032a21f6141142df0c4a
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-27 23:38:26 +0530

    Add sorting functionality and use async/await

--- backend/graphql/queries/productQueries.js
+++ backend/graphql/queries/productQueries.js
@@ -1,16 +1,38 @@
-import { GraphQLList, GraphQLID } from 'graphql'
+import {
+  GraphQLList,
+  GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLString,
+} from 'graphql'
 
 import ProductType from '../types/ProductType'
 import Product from '../../database/models/product'
 
 const products = {
   type: new GraphQLList(ProductType),
-  resolve: (parent, args) => {
-    const productsList = Product.find().exec()
-    if (!productsList) {
-      throw new Error('Error')
+  args: {
+    orderBy: {
+      type: new GraphQLInputObjectType({
+        name: 'orderProductsBy',
+        fields: {
+          // TODO: Should change it to enum type of 'asc' and 'desc'.
+          name: {
+            type: GraphQLString,
+          },
+          actualPrice: {
+            type: GraphQLString,
+          },
+        },
+      }),
+    },
+  },
+  resolve: async (parent, args) => {
+    try {
+      const productsList = await Product.find({}).sort(args.orderBy)
+      return productsList
+    } catch (err) {
+      console.log('Error occurred in fetching products: ', err)
     }
-    return productsList
   },
 }
 
@@ -21,7 +43,14 @@ const product = {
       type: GraphQLID,
     },
   },
-  resolve: (parent, args) => Product.findById(args.id),
+  resolve: async (parent, args) => {
+    try {
+      const dbProduct = await Product.findById(args.id)
+      return dbProduct
+    } catch (err) {
+      console.log('Error occurred in fetching product by ID: ', err)
+    }
+  },
 }
 
 export { products, product }
