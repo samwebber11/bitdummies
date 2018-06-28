5206d3f69ecbdbc36d4e5082bbf4cf69aa0946b5
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-06-28 14:09:53 +0530

    Change Product Schema for GraphQL

--- backend/graphql/mutations/productMutations.js
+++ backend/graphql/mutations/productMutations.js
@@ -5,11 +5,11 @@ import {
   GraphQLFloat,
   GraphQLID,
   GraphQLList,
-  GraphQLInputObjectType,
 } from 'graphql'
 
 import Product from '../../database/models/product'
 import ProductType from '../types/ProductType'
+import SizeInputType from '../types/SizeInputType'
 
 const addProduct = {
   type: ProductType,
@@ -21,21 +21,7 @@ const addProduct = {
       type: new GraphQLNonNull(GraphQLString),
     },
     size: {
-      type: new GraphQLNonNull(
-        new GraphQLList(
-          new GraphQLInputObjectType({
-            name: 'sizeInput',
-            fields: {
-              label: {
-                type: GraphQLString,
-              },
-              quantityAvailable: {
-                type: GraphQLInt,
-              },
-            },
-          })
-        )
-      ),
+      type: new GraphQLNonNull(new GraphQLList(SizeInputType)),
     },
     description: {
       type: new GraphQLNonNull(GraphQLString),
@@ -58,13 +44,14 @@ const addProduct = {
       type: new GraphQLNonNull(GraphQLString),
     },
   },
-  resolve: (parent, args) => {
-    const product = new Product(args)
-    const savedProduct = product.save()
-    if (!savedProduct) {
-      throw new Error('Error')
+  resolve: async (parent, args) => {
+    try {
+      let product = new Product(args)
+      product = await product.save()
+      return product
+    } catch (err) {
+      console.log('Error occurred in adding product: ', err)
     }
-    return savedProduct
   },
 }
 
@@ -75,12 +62,13 @@ const removeProduct = {
       type: new GraphQLNonNull(GraphQLID),
     },
   },
-  resolve: (parent, args) => {
-    const removedProduct = Product.findByIdAndRemove(args.id).exec()
-    if (!removedProduct) {
-      throw new Error('Error')
+  resolve: async (parent, args) => {
+    try {
+      const removedProduct = await Product.findByIdAndRemove(args.id)
+      return removedProduct
+    } catch (err) {
+      console.log('Error occurred in removing product: ', err)
     }
-    return removedProduct
   },
 }
 
@@ -97,21 +85,7 @@ const updateProduct = {
       type: new GraphQLNonNull(GraphQLString),
     },
     size: {
-      type: new GraphQLNonNull(
-        new GraphQLList(
-          new GraphQLInputObjectType({
-            name: 'sizeInputUpdate',
-            fields: {
-              label: {
-                type: GraphQLString,
-              },
-              quantityAvailable: {
-                type: GraphQLInt,
-              },
-            },
-          })
-        )
-      ),
+      type: new GraphQLNonNull(new GraphQLList(SizeInputType)),
     },
     description: {
       type: new GraphQLNonNull(GraphQLString),
@@ -134,24 +108,30 @@ const updateProduct = {
       type: new GraphQLNonNull(GraphQLString),
     },
   },
-  resolve: (parent, args) =>
-    Product.findByIdAndUpdate(
-      args.id,
-      {
-        $set: {
-          name: args.name,
-          category: args.category,
-          size: args.size,
-          description: args.description,
-          actualPrice: args.actualPrice,
-          discount: args.discount,
-          tax: args.tax,
-          imagePath: args.imagePath,
-          delicacy: args.delicacy,
+  resolve: async (parent, args) => {
+    try {
+      const product = await Product.findByIdAndUpdate(
+        args.id,
+        {
+          $set: {
+            name: args.name,
+            category: args.category,
+            size: args.size,
+            description: args.description,
+            actualPrice: args.actualPrice,
+            discount: args.discount,
+            tax: args.tax,
+            imagePath: args.imagePath,
+            delicacy: args.delicacy,
+          },
         },
-      },
-      { new: true }
-    ).catch(err => new Error(err)),
+        { new: true }
+      )
+      return product
+    } catch (err) {
+      console.log('Error occurred in updating product: ', err)
+    }
+  },
 }
 
 export { addProduct, removeProduct, updateProduct }
