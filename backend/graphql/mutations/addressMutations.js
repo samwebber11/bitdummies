7206af0e91326e3e7caf38362dd2c3a70d1a60ee
Kumar Sambhav jain <30227278+samwebber11@users.noreply.github.com>
2018-06-30 19:39:27 +0530

    Update AddressMutations

--- backend/graphql/mutations/addressMutations.js
+++ backend/graphql/mutations/addressMutations.js
@@ -81,38 +81,38 @@ const removeAddress = {
   resolve: async (parent, args, context) => {
     if (context.user) {
       const userId = context.user._id
-    try {
-      let user = User.findById(userId)
-      if (!user) {
-        throw new Error('Could not find user')
-      }
-      // Mapping all the addressIds associated with a particular user to have a check if there
-      // is any address associated with the user. If no address is found then an error occurs otherwise
-      // check the following address in the list of addresses.
-      const addressIds = user.map(address => address.address)
-      if (addressIds.length === 0) {
-        throw new Error(
-          'Could not find any address associated with the current user'
-        )
-      }
-      // TODO: Check if this functions works right away
-      const id = args.id.valueOf()
-      const removeaddress = await Address.findByIdAndRemove(args.id)
-      if (!removeaddress) {
-        throw new Error('Error occured in removing address')
-      }
-      user = await User.findByIdAndUpdate(userId, {
-        address: addressIds.splice(addressIds.indexOf(id), 1),
-      })
-      // const dbAddress = await User.find((args.id: { $in: addressIds }))
+      try {
+        let user = User.findById(userId)
+        if (!user) {
+          throw new Error('Could not find user')
+        }
+        // Mapping all the addressIds associated with a particular user to have a check if there
+        // is any address associated with the user. If no address is found then an error occurs otherwise
+        // check the following address in the list of addresses.
+        const addressIds = user.map(address => address.address)
+        if (addressIds.length === 0) {
+          throw new Error(
+            'Could not find any address associated with the current user'
+          )
+        }
+        // TODO: Check if this functions works right away
+        const id = args.id.valueOf()
+        const removeaddress = await Address.findByIdAndRemove(args.id)
+        if (!removeaddress) {
+          throw new Error('Error occured in removing address')
+        }
+        user = await User.findByIdAndUpdate(userId, {
+          address: addressIds.splice(addressIds.indexOf(id), 1),
+        })
+        // const dbAddress = await User.find((args.id: { $in: addressIds }))
 
-      return removeaddress
-    } catch (err) {
-      console.log('Error occured in removing address: ', err)
-      throw new Error(err)
+        return removeaddress
+      } catch (err) {
+        console.log('Error occured in removing address: ', err)
+        throw err
+      }
     }
   },
-}
 }
 
 const updateAddress = {
@@ -143,30 +143,42 @@ const updateAddress = {
       type: new GraphQLNonNull(GraphQLString),
     },
   },
-  resolve: async (parent, args) => {
+  resolve: async (parent, args, context) => {
     if (context.user) {
       const userId = context.user._id
-    }
-    try {
-      const address = await Address.findByIdAndUpdate(
-        args.id,
-        {
-          $set: {
-            address1: args.address1,
-            address2: args.address2,
-            landmark: args.landmark,
-            city: args.city,
-            state: args.state,
-            zip: args.zip,
-            country: args.country,
+      try {
+        const user = User.findById(userId)
+        if (!user) {
+          throw new Error('No user is found with this id')
+        }
+        // Checking if the address id exists in the id lists of the user.
+        // if found no id then it will throw an error otherwise update the
+        // address at that place.
+        const addressIds = user.map(address => address.address)
+        const dbAddress = await User.find({ args: { $in: addressIds } })
+        if (dbAddress.length !== 1) {
+          throw new Error('Address is not present in the database')
+        }
+        const address = await Address.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              address1: args.address1,
+              address2: args.address2,
+              landmark: args.landmark,
+              city: args.city,
+              state: args.state,
+              zip: args.zip,
+              country: args.country,
+            },
           },
-        },
-        { new: true }
-      )
-      return address
-    } catch (err) {
-      console.log('Error occurred in updating address: ', err)
-      throw err
+          { new: true }
+        )
+        return address
+      } catch (err) {
+        console.log('Error occurred in updating address: ', err)
+        throw err
+      }
     }
   },
 }
