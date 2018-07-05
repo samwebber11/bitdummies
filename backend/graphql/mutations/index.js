e203d9e28c7b371b5aa512023c7ac75a9bbb6e63
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-07-05 12:07:31 +0530

    Fix incorrect mutations bug

--- backend/graphql/mutations/index.js
+++ backend/graphql/mutations/index.js
@@ -1,24 +1,36 @@
 import { GraphQLObjectType } from 'graphql'
 
-import { addProduct, removeProduct, updateProduct } from './productMutations'
-import { addOrder, cancelOrder, updateOrder } from './orderMutations'
 import { addAddress, removeAddress, updateAddress } from './addressMutations'
-import { addUser, removeUser, updateUser } from './userMutations'
+import {
+  addOrder,
+  cancelOrder,
+  removeProductFromOrder,
+  changeOrderStatus,
+} from './orderMutations'
+import {
+  addProduct,
+  removeProduct,
+  updateProductInfo,
+  updateProductImages,
+  updateProductQuantity,
+} from './productMutations'
+import { updateUser } from './userMutations'
 
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    addProduct,
-    removeProduct,
-    updateProduct,
-    addOrder,
-    updateOrder,
-    cancelOrder,
     addAddress,
     updateAddress,
     removeAddress,
-    addUser,
-    removeUser,
+    addOrder,
+    cancelOrder,
+    removeProductFromOrder,
+    changeOrderStatus,
+    addProduct,
+    removeProduct,
+    updateProductInfo,
+    updateProductImages,
+    updateProductQuantity,
     updateUser,
   },
 })
