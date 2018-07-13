ecbf4f9a1eef5b69a9d4d8cc136afac363b13f1e
samwebber11 <samjain15291@gmail.com>
2018-07-13 11:07:22 +0530

    User test

--- backend/test/mutations/user.test.js
+++ backend/test/mutations/user.test.js
@@ -18,130 +18,121 @@ import {
 beforeAll(connectMongoose)
 afterAll(disconnectMongoose)
 
-describe('updateUser resolver', () => {
-  const user = {
-    _id: '5b39f7bb26670102359a8c10',
+describe('updateUser resolver', async () => {
+
+  const dummyUser = {
+    provider: {
+      name: 'google',
+      id: Math.floor(Math.random() * 1000000).toString(),
+    },
+    email: 'dexter.jacobi@gmail.com',
+    firstName: 'Sedrick',
+    lastName: 'Gulgowski',
+    // phone: '854-965-758'
   }
 
-  const firstNames = ['Icie', 'Kirk', 'Sophie', 'Quentin', 'Forest']
-  const lastNames = ['Little', 'Hauck', 'Gislason', 'Wiegand', 'Dietrich']
-  const phoneNumbers = [
-    '378-748-5678',
-    '748-191-9467',
-    '848-787-9259',
-    '030-004-7609',
-    '954-259-8960',
-  ]
-
-  const generateFirstName = () => {
-    const firstNameIndex = Math.floor(Math.random() * firstNames.length)
-    return firstNames[firstNameIndex]
-  }
-
-  const generateLastName = () => {
-    const lastNameIndex = Math.floor(Math.random() * lastNames.length)
-    return lastNames[lastNameIndex]
-  }
-
-  const generatePhoneNumber = () => {
-    const phoneIndex = Math.floor(Math.random() * phoneNumbers.length)
-    return phoneNumbers[phoneIndex]
+  const updateUser = {
+    email: 'dexter.jacobi@gmail.com',
+    firstName: 'Icie',
+    lastName: 'Little',
+    phone: '378-748-5678',
   }
 
   it(`Should update user's name`, async () => {
-    expect.assertions(4)
-    const savedUser = await User.findById(user._id)
-    const { firstName, lastName, name } = savedUser
-    const dummyFirstName = generateFirstName()
-    const dummyLastName = generateLastName()
-    const updatedUser = await updateUserResolver(
-      null,
-      { firstName: dummyFirstName, lastName: dummyLastName },
-      { user: savedUser }
-    )
+    expect.assertions(5)
+
+    // Setup
+    const user = await User.create(merge(dummyUser, { roles: ['admin'] }))
+    const userArgs = merge(updateUser, { id: user._id })
+
+    // Actual Test begins
+    const updatedUser = await updateUserResolver(null, userArgs, { user })
 
     expect(updatedUser).toHaveProperty('_id')
-    expect(updatedUser._id).toEqual(savedUser._id)
-    expect(updatedUser.name).not.toEqual(name)
-    expect(updatedUser.name).toEqual(`${dummyFirstName} ${dummyLastName}`)
+    expect(updatedUser._id).toEqual(user._id)
+    expect(updatedUser.name).not.toEqual(`${user.firstName} ${user.lastName}`)
+    expect(updatedUser.name).toEqual(`${updatedUser.firstName} 
+    ${updatedUser.lastName}`)
+    expect(updatedUser.phone).toEqual(`${user.phone}`)
 
     // Cleanup.
-    await User.findByIdAndUpdate(
-      { _id: updatedUser._id },
-      { firstName, lastName },
-      { runValidators: true }
-    )
+    await User.findByIdAndRemove(user._id)
   })
+  // console.log(dummyUser.name)
 
   it(`Should update user's phone`, async () => {
     expect.assertions(5)
-    const savedUser = await User.findById(user._id)
-    const { firstName, lastName, phone } = savedUser
-    const dummyPhoneNumber = generatePhoneNumber()
-    const updatedUser = await updateUserResolver(
-      null,
-      {
-        firstName: savedUser.firstName,
-        lastName: savedUser.lastName,
-        phone: dummyPhoneNumber,
-      },
-      { user: savedUser }
-    )
+    // Setup.
+    const user = await User.create(merge(dummyUser, { roles: ['admin'] }))
+    const userArgs = merge(updateUser, { id: user._id })
+
+    // Actual test begins.
+    const updatedUser = await updateUserResolver(null, userArgs, { user })
 
     expect(updatedUser).toHaveProperty('_id')
-    expect(updatedUser._id).toEqual(savedUser._id)
-    expect(updatedUser.phone).not.toEqual(phone)
-    expect(updatedUser.phone).toEqual(dummyPhoneNumber)
-    expect(updatedUser.name).toEqual(`${savedUser.name}`)
+    expect(updatedUser._id).toEqual(user._id)
+    expect(updatedUser.phone).not.toEqual(user.phone)
+    expect(updatedUser.phone).toEqual('378-748-5678')
+    expect(updatedUser.name).toEqual(`${user.name}`)
 
     // Cleanup.
-    await User.findByIdAndUpdate(
-      { _id: updatedUser._id },
-      { firstName, lastName, phone },
-      { runValidators: true }
-    )
+    await User.findByIdAndRemove(user._id)
   })
 
   it('Should not update when there is no user', async () => {
     expect.assertions(1)
-    const dummyFirstName = generateFirstName()
-    const dummyLastName = generateLastName()
-    const dummyPhoneNumber = generatePhoneNumber()
+    // Setup.
+    const userArgs = updateUser
 
-    await expect(
-      updateUserResolver(
-        null,
-        {
-          firstName: dummyFirstName,
-          lastName: dummyLastName,
-          phone: dummyPhoneNumber,
-        },
-        {}
-      )
-    ).rejects.toThrow(new AuthenticationError())
+    await expect(updateUserResolver(null, userArgs, {})).rejects.toThrow(
+      new AuthenticationError()
+    )
+  })
+
+  it('Should not update a user when the user is not authorized to update the user', async () => {
+    expect.assertions(1)
+    // Setup.
+    const user = await User.create(dummyUser)
+    const userArgs = merge(updateUser, { id: user._id })
+
+    // Actual test begins.
+    await expect(updateUserResolver(null, userArgs, { user })).rejects.toThrow(
+      new AuthorizationError()
+    )
+
+    // Cleanup.
+    await User.findByIdAndRemove(user._id)
   })
 
   it('Should not update when a field is invalid', async () => {
     expect.assertions(1)
-    const savedUser = await User.findById(user._id)
-    const { firstName, lastName } = savedUser
-    const dummyPhoneNumber = 'possimus doloribus ut'
-    await expect(
-      updateUserResolver(
-        null,
-        { firstName, lastName, phone: dummyPhoneNumber },
-        { user: savedUser }
-      )
-    ).rejects.toThrowError(ValidationError)
+    // Setup.
+    const user = await User.create(merge(dummyUser, { roles: ['admin'] }))
+    const userArgs = merge(updateUser, { id: user._id })
+    userArgs.phone = 'posiqueeli sehmad'
+
+    // Actual test begins.
+    await expect(updateUserResolver(null, userArgs, { user })).rejects.toThrow( 
+    ValidationError
+  )
+
+    // Cleanup.
+    await User.findByIdAndRemove(user._id)
   })
 })
 
-describe('changeUserRole resolver', () => {
-  const user = {
-    _id: '5b39f7bb26670102359a8c10',
+describe('changeUserRole resolver', async () => {
+  const dummyUser1 = {
+    provider: {
+      name: 'google',
+      id: Math.floor(Math.random() * 10000000 + 1).toString(),
+    },
+    email: 'dexter.jacobi@gmail.com',
+    firstName: 'Sedrick',
+    lastName: 'Gulgowski',
   }
 
-  const dummyUser = {
+  const dummyUser2 = {
     provider: {
       name: 'google',
       id: Math.floor(Math.random() * 10000000 + 1).toString(),
@@ -152,19 +143,19 @@ describe('changeUserRole resolver', () => {
     roles: ['user'],
   }
 
+
   it(`Should update a user's roles`, async () => {
     expect.assertions(4)
-    const savedUser = await User.findById(user._id)
-    const newUser = await User.create(dummyUser)
-
+    // Setup.
+    const user = await User.create(dummyUser1)
+    const newUser = await User.create(dummyUser2)
     const args = {
       id: newUser._id,
       roles: ['user', 'admin'],
     }
 
-    const updatedUser = await changeUserRoleResolver(null, args, {
-      user: savedUser,
-    })
+    // Actual test begins.
+    const updatedUser = await changeUserRoleResolver(null, args, { user })
 
     expect(updatedUser).toHaveProperty('roles')
     expect(updatedUser.roles).toHaveLength(2)
@@ -172,18 +163,19 @@ describe('changeUserRole resolver', () => {
     expect(updatedUser.roles).toContain('admin')
 
     // Cleanup.
-    await User.findByIdAndRemove(newUser._id)
+    await User.deleteMany({ id: { $in: [newUser._id, user._id]}})
   })
 
   it(`Should not update a user's roles when there is no user to authorize`, async () => {
     expect.assertions(1)
-    const newUser = await User.create(dummyUser)
-
+    // Setup.
+    const newUser = await User.create(dummyUser2)
     const args = {
       id: newUser._id,
       roles: ['user', 'admin'],
     }
 
+    // Actual test begins.
     await expect(changeUserRoleResolver(null, args, {})).rejects.toThrow(
       new AuthenticationError()
     )
@@ -194,16 +186,17 @@ describe('changeUserRole resolver', () => {
 
   it(`Should not update a user's roles when the user trying to change user's role is not authorized`, async () => {
     expect.assertions(1)
+    // Setup.
     const unauthorizedUser = await User.create(
-      merge(dummyUser, { email: 'Antonetta_Prohaska94@yahoo.com' })
+      merge(dummyUser2, { email: 'Antonetta_Prohaska94@yahoo.com' })
     )
-    const newUser = await User.create(dummyUser)
-
+    const newUser = await User.create(dummyUser2)
     const args = {
       id: newUser._id,
       roles: ['user', 'admin'],
     }
 
+    // Actual test begins
     await expect(
       changeUserRoleResolver(null, args, { user: unauthorizedUser })
     ).rejects.toThrow(new AuthorizationError())
@@ -214,75 +207,73 @@ describe('changeUserRole resolver', () => {
 
   it(`Should not update a user's roles when roles aren't provided`, async () => {
     expect.assertions(1)
-    const savedUser = await User.findById(user._id)
-    const newUser = await User.create(dummyUser)
+    // Setup.
+    const user = await User.create(dummyUser1)
+    const newUser = await User.create(dummyUser2)
 
     const args = {
       id: newUser._id,
     }
 
-    await expect(
-      changeUserRoleResolver(null, args, {
-        user: savedUser,
-      })
-    ).rejects.toThrow(new InvalidRolesError())
+    // Actual test begins.
+    await expect(changeUserRoleResolver(null, args, { user })).rejects.toThrow(
+      new InvalidRolesError()
+    )
 
     // Cleanup.
-    await User.findByIdAndRemove(newUser._id)
+    await User.deleteMany({ _id: { $in: [user._id, newUser._id] }})
   })
 
   it(`Should not update a user's roles when 'roles' field is an empty array`, async () => {
     expect.assertions(1)
-    const savedUser = await User.findById(user._id)
-    const newUser = await User.create(dummyUser)
+    // Setup.
+    const user = await User.create(dummyUser1)
+    const newUser = await User.create(dummyUser2)
 
     const args = {
       id: newUser._id,
       roles: [],
     }
 
+    // Actual test begins.
     await expect(
-      changeUserRoleResolver(null, args, {
-        user: savedUser,
-      })
+      changeUserRoleResolver(null, args, { user })
     ).rejects.toThrowError(ValidationError)
 
     // Cleanup.
-    await User.findByIdAndRemove(newUser._id)
+    await User.deleteMany({ _id: { $in: [newUser._id, user._id] }})
   })
 
   it(`Should not update a user's roles when roles aren't one of the predefined roles`, async () => {
     expect.assertions(1)
-    const savedUser = await User.findById(user._id)
-    const newUser = await User.create(dummyUser)
-
+    // Setup.
+    const user = await User.create(dummyUser1)
+    const newUser = await User.create(dummyUser2)
     const args = {
       id: newUser._id,
       roles: ['blah', 'lorem', 'ipsum'],
     }
-
+    // Actual test begins.
     await expect(
-      changeUserRoleResolver(null, args, {
-        user: savedUser,
-      })
+      changeUserRoleResolver(null, args, { user })
     ).rejects.toThrowError(ValidationError)
 
     // Cleanup.
-    await User.findByIdAndRemove(newUser._id)
+    await User.deleteMany({ _id: { $in: [newUser._id, user._id] }})
   })
 
   it(`Should not update a user's roles when ID is invalid`, async () => {
     expect.assertions(1)
-    const savedUser = await User.findById(user._id)
+    // Setup.
+    const user = await User.create(dummyUser1)
 
     const args = {
       id: new Types.ObjectId(),
       roles: ['user', 'admin'],
     }
 
-    const updatedUser = await changeUserRoleResolver(null, args, {
-      user: savedUser,
-    })
+    // Actual test begins
+    const updatedUser = await changeUserRoleResolver(null, args, { user })
     expect(updatedUser).toBeNull()
   })
 })
